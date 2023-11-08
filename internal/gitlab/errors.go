03e8b017296a528738510c533d4c9c86476f569c
Niv Keidan <nivkeidan@gmail.com>
2023-11-08 13:57:00 +0200

    revert back to empty error string

--- internal/gitlab/errors.go
+++ internal/gitlab/errors.go
@@ -7,25 +7,8 @@ import (
 
 var ErrMissingVar = errors.New("missing environment variable")
 
-// only use for errors.Is() calls. For wrapping, use TransientError().
-var ErrTransient = fmt.Errorf("transient error")
-
-type transientError struct {
-	e error
-}
-
-func (e transientError) Error() string {
-	return e.e.Error()
-}
-
-func (e transientError) Unwrap() error {
-	return e.e
-}
-
-func (e transientError) Is(target error) bool {
-	return target == ErrTransient
-}
+var ErrTransient = errors.New("") // message is empty to avoid showing unnecessary information to the user
 
 func TransientError(e error) error {
-	return transientError{e: e}
+	return fmt.Errorf("%w%w", ErrTransient, e)
 }
